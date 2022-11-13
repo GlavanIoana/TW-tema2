c04f8b6b5764e703214b81fc0994582c1c2818c8
GlavanIoana <102909585+GlavanIoana@users.noreply.github.com>
2022-11-13 23:28:58 +0200

    Add files via upload

---
+++ tema2_GlavanIoana1084/main/app.js
@@ -0,0 +1,29 @@
+function addTokens(input, tokens){
+
+    if(typeof input !== 'string')// || !(input instanceof String)) 
+        {throw Error('Input should be a string');}
+
+    if(input.length < 6) {
+        throw Error('Input shoud have at leat 6 characters');
+    }
+
+    // function isString(token){
+    //     return typeof token.tokenName==='string';}
+    // const isStringsArray =tokens.every(isString);
+    // if(isStringsArray==false){
+    // throw Error('Invalid array format!');}
+
+    let containsdots=input.includes("...");
+    if(!containsdots) {return input;}
+
+    let newStr;
+    for(i=0;i<tokens.length;i++){
+        newStr=input.replace("...",'${'+tokens[i].tokenName+'}');}
+    return newStr;
+}
+
+const app = {
+    addTokens: addTokens
+}
+
+module.exports = app;
